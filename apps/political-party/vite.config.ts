b7fa9ddb1ed1ecba76787ae75a039efee031494d
shagai-rgb <dash.altanshagai48@gmail.com>
2026-01-30 17:11:11 +0800

    Add 5 project rewrites for Vercel

--- apps/political-party/vite.config.ts
+++ apps/political-party/vite.config.ts
@@ -1,28 +1,17 @@
-import tailwindcss from "@tailwindcss/vite";
-// Plugins
-import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
-// import eslintPlugin from 'vite-plugin-eslint'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    // eslintPlugin({
-    //   cache: false,
-    //   include: ['./src/**/*.ts', './src/**/*.tsx'],
-    //   exclude: []
-    // })
-  ],
+  base: '/political-party/',
+  plugins: [react(), tailwindcss()],
   server: {
-    port: 4000,
+    port: 4000
   },
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
-    },
-  },
-});
+      '@': resolve(__dirname, './src')
+    }
+  }
+})
